b43532f66a688689c0d94b2378bea8a0bbce50e2
RodrigoSuar <129218970+RodrigoSuar@users.noreply.github.com>
2023-04-01 01:12:23 -0500

    added file to store list

--- main.cpp
+++ main.cpp
@@ -6,5 +6,11 @@
 using namespace std;
 
 int main(){
-    
+   fstream file("todoList.txt",ios :: in | ios :: out );
+   if (file){
+   
+   }else {
+        cout << "File failed to open" << endl;
+        return 1;
+   }
 }
