f1dcb70c7fd39b27cab1c6ef133825fb93d71662
RodrigoSuar <suarez.rodrigo3004@gmail.com>
2023-04-02 23:48:00 -0500

    Added more functionality to adding to the list

--- main.cpp
+++ main.cpp
@@ -3,13 +3,18 @@
 #include <memory>
 #include <cmath>
 #include <string>
+#include <conio.h>
 using namespace std;
+enum importance{green,yello,red};
 struct node{
     string content;
     node *next = nullptr;
+    importance level;
+    
 };
 void addToList(node*&,fstream&);
 bool isEmpty(fstream&);
+bool isType(string);
 int main(){;
     while(true){
         cout << "What would you like to do?" << endl << endl;
@@ -46,7 +51,6 @@ int main(){;
             addToList(head,file);
 
         }else if (input == '2'){
-
         }else if(input == '3'){
             return 0;
         }else {
@@ -70,12 +74,56 @@ bool isEmpty(fstream& file){
     return false;
 }
 void addToList(node *&head, fstream& file){
+    node *ptr = new node;
     cout << "What would you like to add" << endl << endl;
     string line;
     do{
         getline(cin,line);
     }while(line.empty());
+
+    ptr->content = line;
     file << line << endl;
+    cout << "Add a level of importance" << endl;
+    cout << "Red is the most important. Green is the least." << endl;
+    string level;
+    cin >> level;
+    do{
+    if(level == "red" || level == "Red"){
+        importance lvl = red;
+        ptr->level = lvl;
+    }else if(level == "green" || level == "Green"){
+        importance lvl = green;
+        ptr->level = lvl;
+    }else if(level == "Yellow" || level == "yellow"){
+        importance lvl = yello;
+        ptr->level = lvl;
+    }else {
+        cout << "you entered a wrong type of importance level." << endl;
+        cout << "Please enter a new level." << endl;
+
+    }
+    }while(!isType(level));
+    node *trav = head;
+    if(head == nullptr){
+        head = ptr;
+    }else{
+        while (trav->next != nullptr){
+            trav = trav->next;
+        }
+        trav = ptr;
+    }
+
 
     
+}
+bool isType(string level){
+    if(level == "red" || level == "Red"){
+        return true;
+    }else if(level == "green" || level == "Green"){
+        return true;
+    }else if(level == "Yellow" || level == "yellow"){
+        return true;
+    }else {
+        return false;
+    }
 }
